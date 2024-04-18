f6940e38b65545a97f08a7a0f4202b43c369f460
Daniel Silva <danielhull.silva1@gmail.com>
2024-04-18 22:52:35 +0000

    Programa Unit alterado

--- GameUnits/Unit.cs
+++ GameUnits/Unit.cs
@@ -1,14 +1,22 @@
+using System;
+
 namespace GameUnits
 {
-    public class Unit
+    public abstract class Unit
     {
         private int movement;
         public int Health { get; set; }
+        public abstract float Cost { get; }
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine($"A unit with {movement} movement has moved.");
         }
     }
 }
